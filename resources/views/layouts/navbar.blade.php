b211959848859ab7daf88900596ababbb3f2d4f6
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2022-08-28 18:27:52 -0500

    Actualización de foto de perfil

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -161,7 +161,9 @@
                 <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="avatar-container">
                         <div class="avatar avatar-sm avatar-indicators avatar-online">
-                            <img alt="avatar" src="/assets/img/profile-30.png" class="rounded-circle">
+                            <img class=" rounded-circle" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+
+                            {{-- <img alt="avatar" src="/assets/img/profile-30.png" class="rounded-circle"> --}}
                         </div>
                     </div>
                 </a>
