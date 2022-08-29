84b028cb4bd5be21e78fe654c02e7b035fe2c44f
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2022-08-29 01:03:46 -0500

    Ejemplo de graficos

--- resources/views/purchases/report/index.blade.php
+++ resources/views/purchases/report/index.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.panel')
 
 @section('content')
-    
+<div class="row layout-top-spacing">
+
+<div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+
+     <div class="card"> 
+        <div class="card-body"> 
+
+    <div class="widget widget-chart-two">
+        <div class="widget-heading">
+            <h5 class="">Sales by Category</h5>
+        </div>
+        <div class="widget-content">
+            <div id="" class="">
+                <canvas id="myChart" width="400" height="400"></canvas>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+</div>
+</div>
+<script src="../src/plugins/src/apex/apexcharts.min.js"></script>
+
 @endsection
