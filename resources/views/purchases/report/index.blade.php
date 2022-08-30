8e14cc1f62123f547d7adaa81dd89c13a874b626
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2022-08-30 00:37:16 -0500

    Cambios en los detalles de reporte de Compras

--- resources/views/purchases/report/index.blade.php
+++ resources/views/purchases/report/index.blade.php
@@ -46,7 +46,7 @@
 
 
 
-      <div class="col-xl-3 col-md-6 mb-4">
+      <div class="col-xl-4 col-md-6 mb-4">
         <div class="card border-left-primary shadow h-100 py-2" style="border-left: 0.25rem solid #3d9970!important">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -56,7 +56,7 @@
                         <div class="h3 mb-0 font-weight-bold text-gray-800">10</div>
                     </div>
                     <div class="col-auto">
-                        <i class="fas fa-calendar-alt fa-2x " style="color:white!important"></i>
+                        {{-- <i class="fas fa-calendar-alt fa-2x " style="color:white!important"></i> --}}
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
                     </div>
                 </div>
@@ -66,18 +66,19 @@
 
 
     
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-left-primary shadow h-100 py-2" style="border-left: 0.25rem solid #3d9970!important">
           <div class="card-body">
               <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
                       <div class="text-xm font-weight-bold text-uppercase mb-1" style="color: #4C5755">
                           Compras del Mes</div>
-                      <div class="h3 mb-0 font-weight-bold text-gray-800"></div>
+                      <div class="h3 mb-0 font-weight-bold text-gray-800">100</div>
                   </div>
                   <div class="col-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
 
-                    <i class="fas fa-calendar-times fa-2x " style="color:#3d9970"></i>
+                    {{-- <i class="fas fa-calendar-times fa-2x " style="color:#3d9970"></i> --}}
                   </div>
               </div>
           </div>
@@ -86,18 +87,18 @@
 
 
 
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-left-primary shadow h-100 py-2" style="border-left: 0.25rem solid #3d9970!important">
           <div class="card-body">
               <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
                       <div class="text-xm font-weight-bold text-uppercase mb-1" style="color: #4C5755">
-                        Puntualidades</div>
-                      <div class="h3 mb-0 font-weight-bold text-gray-800"></div>
+                        Gastos de Compras</div>
+                      <div class="h3 mb-0 font-weight-bold text-gray-800">s/.500000.000</div>
                   </div>
                   <div class="col-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
 
-                    <i class="fas fa-clock fa-2x " style="color:#3d9970"></i>
                   </div>
               </div>
           </div>
@@ -105,7 +106,7 @@
     </div>
 
 
-    <div class="col-xl-3 col-md-6 mb-4">
+    {{-- <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-primary shadow h-100 py-2" style="border-left: 0.25rem solid #3d9970!important">
           <div class="card-body">
               <div class="row no-gutters align-items-center">
@@ -121,7 +122,7 @@
               </div>
           </div>
       </div>
-    </div>
+    </div> --}}
   </div>
 
  <div class="row">
@@ -150,7 +151,7 @@
       </div>
       <div class="card-body">
           <div class="chart-pie pt-4 pb-2">
-              <canvas id="mychartardies"></canvas>
+              <canvas id="myChartPai"></canvas>
           </div>
           <br>
       </div>
@@ -165,6 +166,8 @@
 @endsection
 
 @section('scripts')
+
+
 
 <script>
     const ctx = document.getElementById('myChart').getContext('2d');
@@ -204,5 +207,31 @@
     });
 </script>
 
+<script>
+        $(document).ready(function(){
 
+    const ctx = document.getElementById('myChartPai').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'pie',
+        data : {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+}
+    });
+});
+
+</script>
 @endsection
