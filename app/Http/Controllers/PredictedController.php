b80d9e14d47ce10407d76d24de665e5f59b48370
boguirre <boguirre@gmail.com>
2022-11-10 23:58:01 -0500

    www

--- app/Http/Controllers/PredictedController.php
+++ app/Http/Controllers/PredictedController.php
@@ -11,7 +11,7 @@ class PredictedController extends Controller
 {
     public function index(){
 
-        $response = Http::post('http://127.0.0.1:5000/katana-ml/api/v1.0/forecast/ironsteel', [
+        $response = Http::post('https://appflaskccx.onrender.com/katana-ml/api/v1.0/forecast/ironsteel', [
             'horizon' => '12',
         ]);
 
@@ -23,7 +23,7 @@ class PredictedController extends Controller
     }
 
     public function predicted(Request $request){
-        $response = Http::post('http://127.0.0.1:5000/katana-ml/api/v1.0/forecast/ironsteel', [
+        $response = Http::post('https://appflaskccx.onrender.com/katana-ml/api/v1.0/forecast/ironsteel', [
             'horizon' => $request->number,
         ]);
 
